d1a5ec07938ffc81ed808d2ce952d4775ebadc82
MUTH Boravy <muthboravy007@gmail.com>
2026-01-12 21:07:19 +0700

    Compare neutron flux in two reactor regions

--- src/generate_flux_data.py
+++ src/generate_flux_data.py
@@ -7,18 +7,23 @@ np.random.seed(42)
 x = np.linspace(0,100,100)
 
 # Neutron flux model 
-phi_0 = 1e13    # n/cm^2/s 
-L = 30          # diffusion length (cm)
-noise = np.random.normal(0,1e11,size=len(x))
+phi_0 = 1e13  # Initial neutron flux in n/cm²/s
+L1 = 20  # Region 1 diffusion length in cm
+L2 = 50  # Region 2 diffusion length in cm
 
-flux = phi_0*np.exp(-x/L)+noise 
+noise1 = np.random.normal(0,1e11, size=len(x))
+noise2 = np.random.normal(0,1e11, size=len(x)) 
 
-# create dataframe
+flux_region1 = phi_0 * np.exp(-x/L1) + noise1 
+flux_region2 = phi_0 * np.exp(-x/L2) + noise2
+
 df = pd.DataFrame({
     "position_cm": x,
-    "neutron_flux": flux
+    "neutron_flux_region1": flux_region1,
+    "neutron_flux_region2": flux_region2
 })
 
-# save to csv
-df.to_csv("data/neutron_flux.csv", index = False)
-print("Data saved to data/neutron_flux.csv")
+df.to_csv("data/neutron_flux_data.csv", index=False)
+print("Data saved to data/neutron_flux_data.csv")
+
+
